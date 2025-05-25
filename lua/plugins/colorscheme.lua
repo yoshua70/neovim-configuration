db0b355e7da659a20dcf0d8560de5cfd14910a2a
Michee <allidjinoumichee@outlook.fr>
2025-05-25 18:57:11 +0200

    feat: initial commit
    Set up lazy.nvim.
    Add todo-comments plugin so that plugins folder was not empty when
    initializing lazy.nvim.
    Add solarized-osaka theme and set it to be the default theme.
    Display line numbers.
    Set tabulation's width to 4 spaces.

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function()
+        return {
+            transparent = true,
+        }
+    end,
+    config = function()
+        vim.cmd([[colorscheme solarized-osaka]])
+    end,
+}
