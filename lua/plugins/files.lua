b28bcbd24495c130dc13fa57d8d6e975b655e0d0
Michee <allidjinoumichee@outlook.fr>
2025-05-26 22:12:05 +0200

    feat: swap nvimtree for neotree
    Make Neotree open on the right side.

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -1,16 +1,22 @@
 return {
     {
-        "nvim-tree/nvim-tree.lua",
-        version = "*",
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
         lazy = false,
         dependencies = {
+            "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
         },
-        config = function()
-            require("nvim-tree").setup({})
-        end,
+        ---@module "neo-tree
+        ---@type neotree.Config?
         keys = {
-            { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Nvim Tree" },
+            { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Neotree toggle" },
+        },
+        opts = {
+            window = {
+                position =  "right",
+            },
         },
     },
 }
