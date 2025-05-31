ec5917667ec1324ed006b77c508cd6fe772a3ae8
Michee <allidjinoumichee@outlook.fr>
2025-05-31 08:57:00 +0200

    feat: update plugins configuration

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -1,4 +1,5 @@
 return {
+    -- File explorer
     {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
@@ -16,6 +17,32 @@ return {
         opts = {
             window = {
                 position =  "right",
+            },
+        },
+    },
+    -- Telescope - file finder
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        keys = {
+            { 
+                "<leader>ff",
+                "<cmd>Telescope find_files<CR>",
+                "n",
+                desc = "Telescope find file"
+            },
+            { 
+                "<leader>fg",
+                "<cmd>Telescope live_grep<CR>",
+                "n",
+                desc = "Telescope live grep"
+            },
+            { 
+                "<leader>fb",
+                "<cmd>Telescope buffers<CR>",
+                "n",
+                desc = "Telescope buffers"
             },
         },
     },
