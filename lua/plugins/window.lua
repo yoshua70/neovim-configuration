c5eee655358c5bcea4d03d3fea290752777c8953
Michee <allidjinoumichee@outlook.fr>
2025-05-31 08:55:56 +0200

    feat: add lualine

---
+++ lua/plugins/window.lua
@@ -0,0 +1,12 @@
+return {
+    { 
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        options = {
+            theme = "horizon",
+        },
+        lazy = false,
+    },
+}
