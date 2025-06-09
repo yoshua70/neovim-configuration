78845122500289aa519f14cf9a236fa29868e57f
Michee <allidjinoumichee@outlook.fr>
2025-06-09 08:12:21 +0200

    feat: update colorscheme and improve keymaps desc

--- lua/plugins/base_plugins.lua
+++ lua/plugins/base_plugins.lua
@@ -1,19 +1,40 @@
 return {
     { "echasnovski/mini.nvim", version = "*" },
     { "nvim-tree/nvim-web-devicons", opts = {} },
+    -- Colorscheme
     {
         "craftzdog/solarized-osaka.nvim",
-        lazy = false,
-        priority = 1000,
+        -- lazy = false,
+        -- priority = 1000,
         opts = function()
             return {
                 transparent = true,
             }
         end,
         config = function()
-            vim.cmd([[colorscheme solarized-osaka]])
+            -- vim.cmd([[colorscheme solarized-osaka]])
         end,
     },
+    {
+        "projekt0n/github-nvim-theme",
+        name = "github-theme",
+        -- lazy = false,
+        -- priority = 1000,
+        config = function()
+            -- require("github-theme").setup({})
+            -- vim.cmd([[colorscheme github_dark_high_contrast]])
+        end,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme kanagawa-dragon]])
+        end,
+    },
+    -- Keybindings hints
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
@@ -41,7 +62,7 @@ return {
         ---@module "neo-tree
         ---@type neotree.Config?
         keys = {
-            { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Neotree toggle" },
+            { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Neotree: toggle" },
         },
         opts = {
             window = {
@@ -59,20 +80,39 @@ return {
                 "<leader>ff",
                 "<cmd>Telescope find_files<CR>",
                 "n",
-                desc = "Telescope find file"
+                desc = "Telescope: find file"
             },
             { 
                 "<leader>fg",
                 "<cmd>Telescope live_grep<CR>",
                 "n",
-                desc = "Telescope live grep"
+                desc = "Telescope: live grep"
             },
             { 
                 "<leader>fb",
                 "<cmd>Telescope buffers<CR>",
                 "n",
-                desc = "Telescope buffers"
+                desc = "Telescope: buffers"
             },
+        },
+    },
+    -- Language integration
+    { "neovim/nvim-lspconfig" },
+    -- Autocompletion
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
+            -- If using vsnip :
+            -- "hrsh7th/cmp-vsnip",
+            -- "hrsh7th/vim-vsnip",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
         },
     },
 }
