31261ec8960eae8db0163bc765278d29664bf055
Michee <allidjinoumichee@outlook.fr>
2025-07-18 00:34:25 +0200

    feat(plugins): add base plugins
    Add the following plugins :
    - kanagawa.nvim (colorscheme)
    - lazy.nvim
    - mini.icons
    - nvim-treesitter
    - nvim-web-devicons
    - oil.nvim
    - which-key.nvim

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -1,3 +1,76 @@
 return {
-    { "folke/which-key.nvim", lazy = true },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
+    {
+        "nvim-lua/plenary.nvim", lazy = true
+    },
+    {
+        "nvim-tree/nvim-web-devicons", opts = {}
+    },
+    {
+        "echasnovski/mini.icons", version = "*"
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000,
+        lazy = false,
+        init = function()
+            vim.cmd("colorscheme kanagawa")
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "master",
+        lazy = false,
+        build = ":TSUpdate",
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
+        keys = {
+            { "<leader>ft", "<CMD>Telescope find_files<CR>", desc = "Find files (telescope)" },
+            { "<leader>fg", "<CMD>Telescope live_grep<CR>", desc = "Live grep (telescope)" },
+            { "<leader>fb", "<CMD>Telescope buffers<CR>", desc = "Buffers (telescope)" },
+            { "<leader>fh", "<CMD>Telescope help_tags<CR>", desc = "Help tags (telescope)" },
+        },
+    },
+    {
+        "stevearc/oil.nvim",
+        --@module "oil"
+        --@type oil.SetupOpts
+        opts = {
+            view_options = {
+                show_hidden = true,
+            },
+        },
+        dependencies = {
+            { "echasnovski/mini.icons", opts = {} },
+        },
+        lazy = false,
+        keys = {
+            { 
+                "<leader>oo",
+                function()
+                    -- "<CMD>Oil --float =expand('%:p:h')<CR>"
+                    vim.api.nvim_command("Oil --float " .. vim.fn.expand('%:p:h'))
+                end,
+                desc = "Open oil in a floating window (oil.nvim)"
+            },
+        },
+    },
 }
